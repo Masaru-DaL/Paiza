11f381b0bc4076ef235d395da4ed9f5ec783a8fd
Masaru <hittec9609@icloud.com>
2022-04-21 12:03:04 +0900

    #13

---
+++ #13_for/index.php
@@ -0,0 +1,17 @@
+<?php
+    $greeting = "Hello world";
+
+    for ($i = 0; $i < 5; $i++) {
+         echo $greeting . ":" . $i . "\n";
+    }
+?>
+
+<!-- 演習問題 -->
+<?php
+    # 1000 回繰り返す
+    $greeting = "Hello paiza\n";
+
+    for ($i = 0; $i < 1000; $i++) {
+         echo  $greeting;
+    }
+?>
