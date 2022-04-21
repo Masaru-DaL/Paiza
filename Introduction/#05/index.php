4930e92dee43174934c0d41f99dc75e1530a268a
Masaru <hittec9609@icloud.com>
2022-04-22 08:58:19 +0900

    #05

---
+++ Introduction/#05/index.php
@@ -0,0 +1,14 @@
+<?php
+// Here your code !
+$rand_num = rand(1,6);
+echo "サイコロの値は" . $rand_num . "です。";
+?>
+
+<!-- 演習問題1 -->
+<?php
+// Here your code !
+
+$rand_num = rand(50,99);
+echo "スライムに" . $rand_num . "のダメージを与えた。";
+?>
+
