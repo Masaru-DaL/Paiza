3bd5853f0d3dd94e5acaf6465ed7f6929028f739
Masaru-create <hittec9609@icloud.com>
2022-04-23 11:28:24 +0900

    #02

---
+++ Introduction/Lesson3/#02/index.php
@@ -0,0 +1,31 @@
+<?php
+// Here your code !
+$input = trim(fgets(STDIN));
+$input2 = trim(fgets(STDIN));
+echo "標準入力値: " . $input . "\n";
+echo "標準入力値: " . $input2 . "\n";
+echo "$input + $input2";
+?>
+
+<!-- 演習問題 -->
+<?php
+$input = trim(fgets(STDIN));
+echo "$input";
+
+?>
+
+<?php
+$input = trim(fgets(STDIN));
+$input2 = trim(fgets(STDIN));
+$input3 = trim(fgets(STDIN));
+
+echo $input . $input2 . $input3;
+?>
+
+<?php
+$input = trim(fgets(STDIN));
+$input2 = trim(fgets(STDIN));
+$input3 = trim(fgets(STDIN));
+
+echo $input * $input2 * $input3;
+?>
