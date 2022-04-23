721d664c39cd9d54036d5f138658aea7c0a9aa61
Masaru-create <hittec9609@icloud.com>
2022-04-23 11:46:23 +0900

    #03

---
+++ Introduction/Lesson3/#03/index.php
@@ -0,0 +1,49 @@
+<?php
+// Here your code !
+$i = 1;
+
+while($i <= 10) {
+    echo $i . "\n";
+    $i++;
+}
+?>
+
+<!-- 演習問題 -->
+<?php
+$i = 1;
+while($i <= 20) {
+    echo $i . "\n";
+    $i++;
+}
+
+?>
+
+<?php
+$i = 10;
+while($i <= 20) {
+    echo $i . "\n";
+    $i++;
+}
+
+?>
+
+<?php
+$input = trim(fgets(STDIN));
+$input2 = trim(fgets(STDIN));
+
+while($input <= $input2) {
+    echo $input . "\n";
+    $input++;
+}
+
+?>
+
+<?php
+$input = trim(fgets(STDIN));
+$i = 1;
+
+while($i <= $input) {
+    echo "眠い。\n";
+    $i++;
+}
+?>
