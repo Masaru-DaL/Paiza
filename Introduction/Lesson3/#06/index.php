a9a56a1bf0dbab9541e78cf3fd4d1b0eced7bd70
Masaru-create <hittec9609@icloud.com>
2022-04-24 15:48:45 +0900

    #06

--- Introduction/Lesson3/#06/index.php
+++ Introduction/Lesson3/#06/index.php
@@ -8,3 +8,10 @@ for ($year = 1989; $year <= 2015; $year++) {
 
 
 <!-- 演習問題 -->
+<?php
+for ($year = 1926; $year <= 1989; $year++) {
+     $shouwa = $year - 1925;
+     echo "西暦" . $year . "年は、昭和" . $shouwa . "年です。\n";
+}
+
+?>
