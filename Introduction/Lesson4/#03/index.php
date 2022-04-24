ff21ea62c690539e68a3d264d011cf05a275af67
Masaru-create <hittec9609@icloud.com>
2022-04-24 22:50:33 +0900

    #03 Q4

--- Introduction/Lesson4/#03/index.php
+++ Introduction/Lesson4/#03/index.php
@@ -41,3 +41,14 @@ $team[1] = "木こり";
 print_r($team);
 ?>
 
+<?php
+// 標準入力から3行分の入力を配列に代入する
+for($i=0; $i < 3; $i++){
+    $team[] = trim(fgets(STDIN));
+}
+// ここから下に記述　
+// $team の冒頭の要素を削除
+unset($team[0]);
+print_r($team);
+?>
+
