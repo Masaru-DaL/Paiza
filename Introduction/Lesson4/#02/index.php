3f2983275da1934b28ac0e87919589deba066021
Masaru-create <hittec9609@icloud.com>
2022-04-24 22:35:07 +0900

    #02 Q4

--- Introduction/Lesson4/#02/index.php
+++ Introduction/Lesson4/#02/index.php
@@ -17,12 +17,19 @@ print_r($array);
 <?php
 $item = array("回復薬", "布の服", "革の盾");
 print_r($item);
-
 ?>
 
 <?php
 $item = array("ロングソード", "ブレードソード", "エクスカリバー");
 print_r($item);
-
 ?>
 
+<?php
+$a = trim(fgets(STDIN));
+$b = trim(fgets(STDIN));
+$c = trim(fgets(STDIN));
+
+// $a,$b,$cを指定の順番で配列$teamに代入してください
+$team = array($b, $c, $a);
+print_r($team);
+?>
