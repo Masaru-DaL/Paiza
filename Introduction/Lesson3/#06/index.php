cdf89f24049cbdfa8fc5e9ea7413d040f2f72057
Masaru-create <hittec9609@icloud.com>
2022-04-24 15:45:26 +0900

    #06

---
+++ Introduction/Lesson3/#06/index.php
@@ -0,0 +1,10 @@
+<?php
+// Here your code !
+for ($year = 1989; $year <= 2015; $year++) {
+    $heisei = $year - 1988;
+     echo "西暦" . $year . "年は、平成" . $heisei . "年です。\n";
+}
+?>
+
+
+<!-- 演習問題 -->
