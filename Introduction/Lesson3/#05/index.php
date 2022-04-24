dac8a4d7e4b7302fabbfe13ab7ed794c213fb93f
Masaru-create <hittec9609@icloud.com>
2022-04-24 15:24:29 +0900

    #05

---
+++ Introduction/Lesson3/#05/index.php
@@ -0,0 +1,48 @@
+<?php
+// for文+1
+// Here your code !
+for ($i = 1; $i <= 10; $i++) {
+     echo $i . "\n";
+}
+
+// for文-1
+for ($i = 10; $i > 0; $i--) {
+     echo $i . "\n";
+}
+
+// while文
+$i = 1;
+while($i <= 10) {
+    echo $i . "\n";
+    $i++;
+}
+?>
+
+<!-- 演習問題 -->
+<?php
+for ($i = 1; $i <= 20; $i++) {
+     // code...
+     echo $i . "\n";
+}
+
+?>
+
+<?php
+for ($i = 20; $i >= 10; $i--) {
+     // code...
+     echo $i . "\n";
+}
+
+?>
+
+<?php
+$input1 = trim(fgets(STDIN));
+$input2 = trim(fgets(STDIN));
+
+for ($i = $input1; $i <= $input2; $i++) {
+     // code...
+     echo $i . "\n";
+}
+?>
+
+
