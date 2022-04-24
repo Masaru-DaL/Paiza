f094e96b2ae47dabf5f2f3adc818f6f05c55e98e
Masaru-create <hittec9609@icloud.com>
2022-04-24 22:48:39 +0900

    #03 Q2

--- Introduction/Lesson4/#03/index.php
+++ Introduction/Lesson4/#03/index.php
@@ -19,4 +19,14 @@ for($i=0; $i < 5; $i++){
 echo $team[2];
 ?>
 
+<?php
+// 標準入力から3行分の入力を配列に代入する
+for($i=0; $i < 3; $i++){
+    $team[] = trim(fgets(STDIN));
+}
+// ここから下に記述　
+// $team に「狩人」という要素追加
+$team[] = "狩人";
+print_r($team);
+?>
 
