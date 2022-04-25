1849cfef4a02ade12bebb2d43f200885075b612d
Masaru-create <hittec9609@icloud.com>
2022-04-25 17:45:10 +0900

    #05 Q1

--- Introduction/Lesson4/#05/index.php
+++ Introduction/Lesson4/#05/index.php
@@ -8,4 +8,17 @@ print_r($array);
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 標準入力から1行データを取得
+$input = trim(fgets(STDIN));
+// $inputの値が空で無ければループする
+while($input){
+    // 配列に$inputの値を追加
+    $array[] = $input;
+    // 標準入力から1行データを取得
+    $input = trim(fgets(STDIN));
+}
+print_r($array);
+?>
+
 
