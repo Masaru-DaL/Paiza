05b46949a5ad3188e0d74c226b5c89314f4d09d7
Masaru <hittec9609@icloud.com>
2022-04-25 13:56:19 +0900

    #04 Q5

--- Introduction/Lesson4/#04/index.php
+++ Introduction/Lesson4/#04/index.php
@@ -44,3 +44,13 @@ for ($i = 0; $i <= $num; $i++) {
 }
 ?>
 
+<?php
+// 今回は自力で全部書いてみよう！
+$input = trim(fgets(STDIN));
+$array = explode(",", $input);
+$num = count($array);
+for ($i = 0; $i < $num; $i++) {
+     echo $array[$i]. "さん\n";
+}
+?>
+
