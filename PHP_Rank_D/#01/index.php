61aae52259f42c4e01b08aaef8d463ddfb1895fa
Masaru <hittec9609@icloud.com>
2022-04-25 12:13:22 +0900

    Organize

--- PHP_Rank_D/#01/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
-    $input_line1 = trim(fgets(STDIN));
-    $input_line2 = trim(fgets(STDIN));
-    echo $input_line1 * $input_line2 . "\n";
-?>
--- PHP_Rank_D/#03/index.php
+++
@@ -1,7 +0,0 @@
-<!-- 表面積を求めよ -->
-<?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
-    $input_line = trim(fgets(STDIN));
-    echo $input_line * $input_line *6;
-?>
