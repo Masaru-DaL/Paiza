6202d5cf81f6b6162fe6caa1c1ba6657e500d71c
Masaru-create <hittec9609@icloud.com>
2022-04-25 21:59:19 +0900

    #07 Q!

--- Introduction/Lesson4/#07/index.php
+++ Introduction/Lesson4/#07/index.php
@@ -10,4 +10,18 @@ echo $member[$key];
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 標準入力から1行取得
+$input = trim(fgets(STDIN));
+// カンマで分割して配列に代入
+$num = explode(",", $input);
+// ランダムの上限値を計算して変数に代入
+$max = count($num)-1;
+// 配列の中身を出力
+$omikuji = rand(0, $max);
+// ランダム出力
+print_r($num);
+echo $num[$omikuji];
+?>
+
 
