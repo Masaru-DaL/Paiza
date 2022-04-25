b5d499427bd037d8ac7d9d745e774d23bcd0d11d
Masaru <hittec9609@icloud.com>
2022-04-25 13:49:29 +0900

    #04 Q3

--- Introduction/Lesson4/#04/index.php
+++ Introduction/Lesson4/#04/index.php
@@ -22,3 +22,11 @@ $array = explode("<>" , $input);
 print_r($array);
 ?>
 
+<?php
+// 標準入力から1行取得し$inputに代入
+$input = trim(fgets(STDIN));
+// カンマ区切りでいくつの要素が入っているか数えよう
+$array = explode("," , $input);
+echo count($array);
+?>
+
