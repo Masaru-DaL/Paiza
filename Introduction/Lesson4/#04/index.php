c51d1ba614fd35e6c76d7fc2078feceed56df0e1
野原 大 <220093@yse.ac.jp>
2022-04-25 13:34:41 +0900

    #04 Q1

--- Introduction/Lesson4/#04/index.php
+++ Introduction/Lesson4/#04/index.php
@@ -6,3 +6,13 @@ print_r($array);
 
 echo count($array);
 ?>
+
+<!-- 演習問題 -->
+<?php
+// 標準入力から1行取得し$inputに代入
+$input = trim(fgets(STDIN));
+// $inputの文字列を分割してみよう！
+$array = explode("/" , $input);
+print_r($array);
+?>
+
