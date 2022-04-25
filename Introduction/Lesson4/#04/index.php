f1ce0a337c953ced45684cdb728e182627baa4b6
Masaru <hittec9609@icloud.com>
2022-04-25 13:53:11 +0900

    #04 Q4

--- Introduction/Lesson4/#04/index.php
+++ Introduction/Lesson4/#04/index.php
@@ -30,3 +30,17 @@ $array = explode("," , $input);
 echo count($array);
 ?>
 
+<?php
+// 標準入力から1行取得し$inputに代入
+$input = trim(fgets(STDIN));
+// explodeで$inputの文字列を分割し$arrayに配列として代入
+$array = explode(",", $input);
+// $arrayの要素数を数えて$numに代入
+$num = count($array);
+// $numの数だけループして$arrayから
+// 要素を一つづつ取り出してみよう
+for ($i = 0; $i <= $num; $i++) {
+     echo $array[$i]. "\n";
+}
+?>
+
