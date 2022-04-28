d0c5a224c393e2a5fc8757909fd7e57ff16e6746
Masaru <hittec9609@icloud.com>
2022-04-28 10:08:23 +0900

    #02 Q1

--- Introduction/Lesson6/#02/index.php
+++ Introduction/Lesson6/#02/index.php
@@ -24,3 +24,19 @@ echo $teams[2][2]. "\n";
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 2次元配列を作成してみよう
+$item1 = ["木の棒", "こん棒"];
+$item2 = ["おにぎり", "おにぎり"];
+$item3 = ["毒消し", "薬草"];
+
+// item1 ~ 3を、$basket配列に代入してください。
+$basket = [$item1, $item2, $item3];
+
+echo $basket[0][0]."\n";
+echo $basket[0][1]."\n";
+echo $basket[1][0]."\n";
+echo $basket[1][1]."\n";
+echo $basket[2][0]."\n";
+echo $basket[2][1]."\n";
+?>
