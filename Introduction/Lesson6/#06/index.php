52f04ecd19464a70834f2f7c0ecb71e16fbdd649
Masaru <hittec9609@icloud.com>
2022-04-28 11:09:38 +0900

    #06 Q1

--- Introduction/Lesson6/#06/index.php
+++ Introduction/Lesson6/#06/index.php
@@ -16,4 +16,20 @@ echo "-----\n";
 ?>
 
 <!-- 演習問題 -->
+<?php
+// この下に配列を作成
+$arr = array(
+    array(7, 7, 7, 7),
+    array(7, 7, 7, 7),
+    array(7, 7, 7, 7)
+);
+
+foreach ($arr as $value) {
+    foreach ($value as $val) {
+        echo $val." ";
+    }
+    echo "\n";
+}
+?>
+
 
