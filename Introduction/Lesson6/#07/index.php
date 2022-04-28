48711dee27f6d01263393f5869d002f0893f29ba
Masaru <hittec9609@icloud.com>
2022-04-28 11:18:03 +0900

    #07 Q1

--- Introduction/Lesson6/#07/index.php
+++ Introduction/Lesson6/#07/index.php
@@ -24,3 +24,25 @@ foreach ($enemyImage as $row) {
 ?>
 
 <!-- 演習問題 -->
+<?php
+// ドットで文字を出力しよう
+$letter_A = array(
+    array(0, 0, 1, 1, 0, 0),
+    array(0, 1, 0, 0, 1, 0),
+    array(1, 0, 0, 0, 0, 1),
+    array(1, 1, 1, 1, 1, 1),
+    array(1, 0, 0, 0, 0, 1),
+    array(1, 0, 0, 0, 0, 1),
+);
+// ここに、ドットを表示するコードを記述する
+foreach ($letter_A as $line) {
+    foreach ($line as $dot) {
+        if ($dot == 1) {
+            echo "@";
+        } else {
+            echo " ";
+        }
+    }
+    echo "\n";
+}
+?>
