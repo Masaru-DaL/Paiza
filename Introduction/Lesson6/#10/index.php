af843305b2bdaddf8061a6f1b1806265317af976
Masaru-create <hittec9609@icloud.com>
2022-04-29 07:09:46 +0900

    #10 Q1

--- Introduction/Lesson6/#10/index.php
+++ Introduction/Lesson6/#10/index.php
@@ -19,3 +19,21 @@ foreach ($landMap as $i => $row) {
 }
 ?>
 
+<!-- 演習問題 -->
+<?php
+// 模様を出力してみよう
+$landMap = array_fill(0, 5, array_fill(0, 10, "."));
+
+foreach ($landMap as $i => $row) {
+    foreach ($row as $j => $column) {
+        // $i, $jどちらも2で割り切れる場合は、「+」を出力
+        // そうで無い場合は$columnを出力
+        if ($i % 2 == 0 && $j % 2 == 0) {
+            echo "+";
+        } else {
+            echo $column;
+        }
+    }
+    echo "\n";
+}
+?>
