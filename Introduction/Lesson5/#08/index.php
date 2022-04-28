a4a541f2761b8793a9dc190b3be69f8c52ad6a50
Masaru <hittec9609@icloud.com>
2022-04-28 09:45:49 +0900

    #08 Q2

--- Introduction/Lesson5/#08/index.php
+++ Introduction/Lesson5/#08/index.php
@@ -30,3 +30,19 @@ foreach ($item_list as $item_name) {
 }
 ?>
 
+<?php
+$item_img = array(
+    "剣" => "http://paiza.jp/learning/images/sword.png",
+    "盾" => "http://paiza.jp/learning/images/shield.png",
+    "回復薬" => "http://paiza.jp/learning/images/potion.png",
+    "クリスタル" => "http://paiza.jp/learning/images/crystal.png"
+);
+// ここから下に記述
+while($input = trim(fgets(STDIN))) {
+    $item_list[] = $input;
+}
+foreach ($item_list as $item_name) {
+    echo "<img src=". $item_img[$item_name]. ">";
+}
+?>
+
