e8c983d05596070b026bfd43763a7176c6d7edd9
Masaru <hittec9609@icloud.com>
2022-04-28 10:56:55 +0900

    #05 Q2

--- Introduction/Lesson6/#05/index.php
+++ Introduction/Lesson6/#05/index.php
@@ -41,3 +41,21 @@ for ($i = 0; $i < count($teams); $i++) {
 }
 ?>
 
+<?php
+// ループで2次元配列を出力してみよう
+
+$teams = [
+  ["勇者", "忍者"],
+  ["武士", "戦士"],
+  ["僧侶", "魔法使い"],
+];
+
+foreach ($teams as $team) {
+  foreach ($team as $member) {
+    // この下に$memberの値を出力するコードを記述
+    echo $member;
+    echo "\n";
+  }
+}
+?>
+
