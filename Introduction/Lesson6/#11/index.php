2d3a2a5dfb7997e673f876c8c365814ad7bc9782
Masaru-create <hittec9609@icloud.com>
2022-04-29 07:17:44 +0900

    #11 Q1

--- Introduction/Lesson6/#11/index.php
+++ Introduction/Lesson6/#11/index.php
@@ -19,3 +19,32 @@ foreach ($table as $line) {
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 標準入力から、2次元配列を読み込む
+/*
+標準入力のデータ
+0,0,1,1,0,0
+0,1,0,0,1,0
+1,0,0,0,0,1
+1,1,1,1,1,1
+1,0,0,0,0,1
+1,0,0,0,0,1
+*/
+
+for ($i = 0; $i < 6; $i++) {
+    // ここに、読み込んだデータを","で分割し、配列に追加するコードを記述する
+    $letter_A[] = explode(",", trim(fgets(STDIN)));
+}
+
+foreach ($letter_A as $row) {
+    foreach ($row as $column) {
+        if ($column == 1) {
+            echo "@";
+        }
+        else {
+            echo " ";
+        }
+    }
+    echo "\n";
+}
+?>
