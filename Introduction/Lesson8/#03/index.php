588f245e02e6ad761d12c50122cea63b2b523972
Masaru-create <hittec9609@icloud.com>
2022-04-29 16:15:45 +0900

    #03 Q2

--- Introduction/Lesson8/#03/index.php
+++ Introduction/Lesson8/#03/index.php
@@ -37,4 +37,21 @@ class Greeting {
 $greeting = new Greeting("paiza");
 $greeting -> sayHello();
 
+<?php
+class Greeting {
+    public $myName;
+
+    public function __construct($name) {
+        $this->myName = $name;
+    }
+
+    public function sayHello() {
+        echo "hello " . $this->myName;
+    }
+}
+
+// この下に、インスタンス生成とメソッド呼び出しを追加する
+$greeting = new Greeting("PHP");
+$greeting -> sayHello();
+?>
 
