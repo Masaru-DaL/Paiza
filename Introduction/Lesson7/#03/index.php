fda9ffd938e39360a124847eb2a085ecaf8a1832
Masaru-create <hittec9609@icloud.com>
2022-04-29 13:06:39 +0900

    #03 Q2

--- Introduction/Lesson7/#03/index.php
+++ Introduction/Lesson7/#03/index.php
@@ -37,3 +37,20 @@ echo multiply(5, 7). "\n";
 echo multiply(12, 34). "\n";
 ?>
 
+<?php
+// 九九の表を作成してみよう
+function multiply($x, $y){
+    return $x * $y;
+}
+
+for ($step = 0; $step < 9; $step ++) {
+    for ($num = 0; $num < 9; $num++) {
+        echo multiply($step + 1, $num + 1);
+        if ($num < 8) {
+            echo " ";
+        }
+    }
+    echo "\n";
+}
+?>
+
