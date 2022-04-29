17fe845b40d222f0ab480e26258bc8562ab6e952
Masaru-create <hittec9609@icloud.com>
2022-04-29 12:57:18 +0900

    #03 Q1

--- Introduction/Lesson7/#03/index.php
+++ Introduction/Lesson7/#03/index.php
@@ -12,4 +12,16 @@ echo $num2 . "\n";
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 掛け算関数を呼び出してみよう
+
+function multiply($x, $y){
+    return $x * $y;
+}
+
+// この下に関数呼び出しを記述する
+$num1 = multiply(3, 4);
+echo $num1;
+?>
+
 
