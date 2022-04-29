2b045571e60f6a2b62ebbac14c5014ead8df79e9
Masaru-create <hittec9609@icloud.com>
2022-04-30 07:46:39 +0900

    #02 Q2

--- Introduction/Lesson9/#02/index.php
+++ Introduction/Lesson9/#02/index.php
@@ -27,4 +27,48 @@ $jewelly_box -> open();
 ?>
 
 <!-- 演習問題 -->
+<?php
+// クラスにメソッドを定義しよう
+class Greeting {
+    public $msg;
+    public $target;
+
+    public function __construct() {
+        $this->msg = "hello";
+        $this->target = "paiza";
+    }
+    public function sayHello() {
+        echo $this->msg." ".$this->target;
+    }
+}
+
+class Hello extends Greeting {
+}
+
+$hello = new Hello();
+// 続けて、sayHelloメソッドを呼び出す
+$hello -> sayHello();
+?>
+
+<?php
+// 間違い探し
+class Greeting {
+    public $msg;
+    public $target;
+
+    public function __construct() {
+        $this->msg = "hello";
+        $this->target = "paiza";
+    }
+}
+
+class Hello extends Greeting {
+    public function sayHello() {
+        echo $this -> msg . " " . $this -> target;
+    }
+}
+
+$player = new Hello($msg, $target);
+$player->sayHello();
+?>
 
