e0253f25e0ebc918f2129fdb14f1f0f78049d6be
Masaru-create <hittec9609@icloud.com>
2022-04-30 07:18:37 +0900

    #07 Q1

--- Introduction/Lesson8/#07/index.php
+++ Introduction/Lesson8/#07/index.php
@@ -20,4 +20,18 @@ echo "合計金額は" . $total . "円です。\n";
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 学生メソッドを呼び出す
+class Gakusei {
+    // この下に、合計得点を戻り値として返すsumメソッドを記述する
+    public static function sum($kokugo, $sansu) {
+        return $kokugo + $sansu;
+    }
+}
+
+// この下に、合計得点を戻り値として返すGakuseiクラスsumメソッドを呼び出す
+// 国語 = 70点、算数 = 43点
+$total = Gakusei::sum(70, 43);
+echo "合計は" . $total . "点です。";
+?>
 
