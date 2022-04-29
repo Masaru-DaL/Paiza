74c9a86c987c817ff0e69aaab09ebd2f22bff21b
Masaru-create <hittec9609@icloud.com>
2022-04-29 13:46:42 +0900

    #05 Q1

--- Introduction/Lesson7/#05/index.php
+++ Introduction/Lesson7/#05/index.php
@@ -18,3 +18,22 @@ function attack($target) {
 ?>
 
 <!-- 演習問題 -->
+<?php
+// RPGの攻撃シーン
+function attack($person){
+    echo $person."はスライムを攻撃した\n";
+}
+
+function output_ememy_hp($enemy_hp){
+    echo "敵のHPは残り".$enemy_hp."です\n";
+}
+
+$enemy_hp = trim(fgets(STDIN));
+$team = array("勇者" => 200, "戦士" => 150, "魔法使い" => 100);
+foreach ($team as $person => $power) {
+    attack($person);
+    $enemy_hp = $enemy_hp -$power;
+    output_ememy_hp($enemy_hp);
+}
+
+?>
