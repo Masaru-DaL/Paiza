25f6a3bd9c3bdf199afba5fa339c183123011f9e
Masaru-create <hittec9609@icloud.com>
2022-04-30 08:02:09 +0900

    #03 Q1

--- Introduction/Lesson9/#03/index.php
+++ Introduction/Lesson9/#03/index.php
@@ -29,3 +29,21 @@ $magicBox -> open();
 ?>
 
 <!-- 演習問題 -->
+<?php
+// メソッドをオーバーライドしよう
+class Greeting {
+    public function sayHello() {
+        echo "greeting paiza";
+    }
+}
+
+class Hello extends Greeting {
+    // この下で、メソッドをオーバーライドする
+    public function sayHello() {
+        echo "hello paiza";
+    }
+}
+
+$player = new Hello();
+$player->sayHello();
+?>
