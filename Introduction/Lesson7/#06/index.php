550683057d5faf779e0fad685a544368178d391d
Masaru-create <hittec9609@icloud.com>
2022-04-29 13:50:46 +0900

    #06

---
+++ Introduction/Lesson7/#06/index.php
@@ -0,0 +1,12 @@
+<?php
+// 引数のデフォルト値
+function intoroduce($name = "村人") {
+    echo "私は" . $name . "です\n";
+}
+intoroduce();
+intoroduce();
+intoroduce("勇者");
+intoroduce("戦士");
+?>
+
+<!-- 演習問題 -->
