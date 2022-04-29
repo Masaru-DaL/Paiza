222ee775e6028105c01c6baec426a96e0bf387d2
Masaru-create <hittec9609@icloud.com>
2022-04-30 06:39:22 +0900

    #04 Q1

--- Introduction/Lesson8/#04/index.php
+++ Introduction/Lesson8/#04/index.php
@@ -24,3 +24,27 @@ foreach ($enemies as $enemy) {
 ?>
 
 <!-- 演習問題 -->
+<?php
+// RPGの攻撃シーン
+class Player {
+    public $myName;
+
+    public function __construct($name) {
+        $this->myName = $name;
+    }
+
+    public function attack($enemy) {
+	    echo $this->myName . "は" . $enemy . "を攻撃した\n";
+    }
+}
+
+$team[] = new Player("勇者");
+$team[] = new Player("戦士");
+$team[] = new Player("魔法使い");
+
+$enemy = "スライム";
+
+foreach ($team as $member) {
+    echo $member -> attack($enemy);
+}
+?>
