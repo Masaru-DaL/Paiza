44d18c5d5e9333afe7ef0703d959c9aa4c29925e
Masaru-create <hittec9609@icloud.com>
2022-04-30 06:52:48 +0900

    #05 Q1

--- Introduction/Lesson8/#05/index.php
+++ Introduction/Lesson8/#05/index.php
@@ -22,4 +22,25 @@ echo "合計金額は" . $orange -> getTotalPrice() . "円です。\n";
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 学生メソッドを呼び出す
+class Gakusei {
+    public $myKokugo;
+    public $mySansu;
+
+    public function __construct($kokugo, $sansu) {
+        $this->myKokugo = $kokugo;
+        $this->mySansu = $sansu;
+    }
+
+    public function sum() {
+        return $this->myKokugo + $this->mySansu;
+    }
+}
+// この下に、クラスをインスタンス化し、メソッド呼び出しを記述する
+$gakusei = new Gakusei(70, 43);
+$total = $gakusei -> sum();
+echo "合計は" . $total . "点です\n";
+?>
+
 
