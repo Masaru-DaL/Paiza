8a1828560a238d6023aa7de6d13b9cfab968c417
Masaru-create <hittec9609@icloud.com>
2022-04-30 07:09:30 +0900

    #06 Q1

--- Introduction/Lesson8/#06/index.php
+++ Introduction/Lesson8/#06/index.php
@@ -20,4 +20,24 @@ echo $player -> myName . "\n";
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 間違い探し
+class Greeting {
+    private $myName;
+
+    public function __construct($name) {
+        $this->myName = $name;
+    }
+
+    private function sayHello() {
+        echo "hello " . $this->myName;
+    }
+    public function sayHelloHello() {
+        $this -> sayHello();
+    }
+}
+
+$paiza = new Greeting("paiza");
+$paiza->sayHelloHello();
+?>
 
