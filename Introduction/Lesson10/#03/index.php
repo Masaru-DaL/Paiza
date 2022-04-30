4c0a1f85b60b5e4f868a1ad249abc215e080ff1a
Masaru-create <hittec9609@icloud.com>
2022-04-30 15:10:36 +0900

    #03

---
+++ Introduction/Lesson10/#03/index.php
@@ -0,0 +1,16 @@
+<?php
+echo "start\n";
+try {
+    $date = new DateTime("199x-01-01");
+    echo $date -> format('Y/m/d') . "\n";
+} catch (Exception $e) {
+    echo "指定された日付が不正です\n";
+    // echo "発生した例外:". $e. "\n";
+    // echo "発生した例外:", $e, "\n";
+    fputs(STDERR, $e -> getMessage() . "\n");
+} finally {
+    echo "end\n";
+}
+?>
+
+<!-- 演習問題 -->
