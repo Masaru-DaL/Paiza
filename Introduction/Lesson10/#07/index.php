58ad0abd9f27899495439769c95a841fca6e63f7
Masaru-create <hittec9609@icloud.com>
2022-05-01 06:58:52 +0900

    #07 Q1

--- Introduction/Lesson10/#07/index.php
+++ Introduction/Lesson10/#07/index.php
@@ -23,4 +23,24 @@ echo "9\n";
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 呼び出し元に例外を伝えよう
+
+function test_exception($date) {
+    try {
+        return new DateTime($date);
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+
+echo "start\n";
+try {
+    $dateTime = test_exception("199x-01-01");
+} catch (Exception $e) {
+    echo "不正な日付です\n";
+} finally {
+    echo "end\n";
+}
+?>
 
