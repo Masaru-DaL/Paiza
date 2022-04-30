ca955aee2ff5164b65eca68195f2094586308986
Masaru-create <hittec9609@icloud.com>
2022-04-30 14:55:58 +0900

    #02 Q1

--- Introduction/Lesson10/#02/index.php
+++ Introduction/Lesson10/#02/index.php
@@ -11,3 +11,15 @@ echo $date -> format('Y/m/d') . "\n";
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 例外処理を定義しよう
+echo "start\n";
+try {
+    $date = new DateTime("199x-01-01");
+    echo $date->format('Y/m/d') . "\n";
+} catch(Exception $e) {
+    echo $e -> getMessage() . "\n";
+} finally {
+    echo "end\n";
+}
+?>
