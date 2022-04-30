fcecd9a809752e782a348662af4fbc978d40207e
Masaru-create <hittec9609@icloud.com>
2022-04-30 15:29:38 +0900

    #05 Q1

--- Introduction/Lesson10/#05/index.php
+++ Introduction/Lesson10/#05/index.php
@@ -11,4 +11,15 @@ try {
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 意図的に例外を発生させてみよう
+echo "start\n";
+try {
+    throw new RangeException("意図的なRangeException");
+} catch(RangeException $e) {
+    fputs(STDERR, "例外発生：" .$e->getMessage() . "\n");
+} finally {
+    echo "end\n";
+}
+?>
 
