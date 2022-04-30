8678feff9b34f5f051ffc216292cba59031bed40
Masaru-create <hittec9609@icloud.com>
2022-04-30 15:25:09 +0900

    #04 Q1

--- Introduction/Lesson10/#04/index.php
+++ Introduction/Lesson10/#04/index.php
@@ -11,3 +11,16 @@ try {
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 意図的に例外を発生させてみよう
+echo "start\n";
+try {
+    // ここで例外を発生させる
+    throw new Exception("意図的な例外");
+        echo "例外が起こった後\n";
+} catch(Exception $e) {
+    fputs(STDERR, "例外発生:" .$e->getMessage() . "\n");
+} finally {
+    echo "end\n";
+}
+?>
