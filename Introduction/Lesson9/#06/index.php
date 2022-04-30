f70f60fabdabd1f039ca4b1a83c14479c413920f
Masaru-create <hittec9609@icloud.com>
2022-04-30 13:58:51 +0900

    #06 Q1

--- Introduction/Lesson9/#06/index.php
+++ Introduction/Lesson9/#06/index.php
@@ -39,3 +39,35 @@ foreach ($party as $member) {
 ?>
 
 <!-- 演習問題 -->
+<?php
+// 親クラスと子クラス
+class Greeting {
+    public $msg;
+    public $target;
+
+
+    public function __construct() {
+        $this->target = "paiza";
+        $this->msg = "hello";
+    }
+
+    public function sayHello() {
+        echo $this->msg." ".$this->target."\n";
+    }
+}
+class NewFriend extends Greeting {
+    public function __construct() {
+        // 下記に足りない処理を記述する
+        parent::__construct();
+        $this->target = "PHP";
+    }
+    public function sayHello() {
+        echo $this->msg." ".$this->target."\n";
+    }
+
+}
+
+$paiza = new Greeting();
+$paiza->sayHello();
+$paiza = new NewFriend();
+$paiza->sayHello();
