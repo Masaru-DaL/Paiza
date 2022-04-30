f0a304206c2106cf90e0347fd4e57df5022aa04c
Masaru-create <hittec9609@icloud.com>
2022-04-30 15:52:08 +0900

    #07

---
+++ Introduction/Lesson10/#07/index.php
@@ -0,0 +1,26 @@
+<?php
+function test_exception($date) {
+    echo "2\n";
+    try {
+        echo "3\n";
+        return new DateTime($date);
+        echo "4\n";
+    } catch (Exception $e) {
+        echo "5\n";
+        throw $e;
+    }
+    echo "6\n";
+}
+
+echo "1\n";
+try {
+    $dateTime = test_exception("199x-01-01");
+    echo "7\n";
+} catch (Exception $e) {
+    echo "8\n";
+}
+echo "9\n";
+?>
+
+<!-- 演習問題 -->
+
