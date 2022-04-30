05a8f7c338faf16bd7de452409aff058c2fba4ea
Masaru-create <hittec9609@icloud.com>
2022-04-30 15:29:04 +0900

    #05

---
+++ Introduction/Lesson10/#05/index.php
@@ -0,0 +1,14 @@
+<?php
+echo "start\n";
+try {
+    throw new LengthException("意図的な例外");
+    echo "例外を投げた後\n";
+} catch (LengthException $e) {
+    echo "例外発生：" . $e->getMessage() . "\n";
+} finally {
+    echo "end\n";
+}
+?>
+
+<!-- 演習問題 -->
+
